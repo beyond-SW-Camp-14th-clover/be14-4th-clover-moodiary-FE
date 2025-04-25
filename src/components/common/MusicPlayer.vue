87538341520951a8fa4f5a0641379cd87aff95c5
50071 <rkd50071@gmail.com>
2025-04-25 16:24:27 +0900

    mp 추가 ㅋㅋ

---
+++ src/components/common/MusicPlayer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="hidden-music">
+        <iframe width="0" height="0"
+            :src="`https://www.youtube.com/embed/${videoId}?autoplay=1&loop=1&playlist=${videoId}&controls=0&mute=0`"
+            frameborder="0" allow="autoplay"></iframe>
+    </div>
+</template>
+
+<script setup>
+const videoId = 'p3dxVQsDwyU' // 재생할 YouTube 영상 ID
+</script>
+
+<style scoped>
+.hidden-music {
+    width: 0;
+    height: 0;
+    overflow: hidden;
+    position: absolute;
+}
+</style>
